025b86b41877a03c2bb91e12a1d78bab663f8155
Soria Cristian <93338552+Csoria30@users.noreply.github.com>
2023-05-17 01:09:16 -0300

    ConexionDB
    Se realiza clase conexion.
    Se agrega drivers MariaDB V3.1.4 (Stable)

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -1,7 +1,59 @@
 //@author Grupo11
 
 package Conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 
 public class Conexion {
+    private static final String DATABASE_URL = "jdbc:mariadb://";
+    private static final String DATABASE_HOST = "localhost";
+    private static final String DATABASE_PUERTO = ":3306";
+    private static final String DATABASE_DB = "/db_universidad";
+    private static final String DATABASE_USUARIO = "root";
+    private static final String DATABASE_PASSWORD = "";
+    private static Conexion conexion = null;
     
+    private Conexion() {
+        try {
+            //Cargar drivers
+            Class.forName("org.mariadb.jdbc.Driver");
+            
+        } catch ( ClassNotFoundException ex ) {
+            JOptionPane.showMessageDialog(null, "Clase Conexion: Error al cargar Driver" + ex);
+        }
+    }
+
+    public static Connection getConexion() {
+        Connection con = null;
+        
+        if ( conexion == null ) {
+            conexion = new Conexion();
+        }
+        
+        try {
+            // Setup the connection with the DB
+            con = DriverManager.getConnection(
+                    DATABASE_URL + 
+                    DATABASE_HOST + 
+                    DATABASE_PUERTO + 
+                    DATABASE_DB + 
+                    "?useLegacyDatetimeCode=false&serverTimezone=UTC" + 
+                    "&user=" + 
+                    DATABASE_USUARIO + 
+                    "&password=" + 
+                    DATABASE_PASSWORD
+            );
+            
+        }catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex.getMessage() );
+        }
+        
+        return con;
+    }
+
 }
